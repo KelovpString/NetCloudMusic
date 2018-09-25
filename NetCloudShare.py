28d910de1533f94c0a207c2eaf51cd9b69419b84
KelovpString <ldxzccr@163.com>
2018-09-25 21:25:44 +0800

    V1.0

--- NetCloudShare.py
+++ NetCloudShare.py
@@ -1,11 +1,18 @@
 import json
 import random
 import urllib.request
-
+import urllib.parse
+from selenium import webdriver
+import time
 base_url = r'http://music.163.com/api/'
 music_list_url = r'user/playlist/?offset=0&limit=64&uid=97752165'
 list_info_url = r'playlist/detail?id='
 commet_url = r'v1/resource/comments/{0}/?rid={1}&\offset=0&total=fasle&limit=100'
+qzone_url = r'https://sns.qzone.qq.com/cgi-bin/qzshare/cgi_qzshare_onekey?'
+request_map = {
+    'site' : '网易云音乐'
+}
+
 head =  {
     'Host':'music.163.com',
     'User-Agent':'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:56.0) Gecko/20100101 Firefox/56.0',
@@ -19,6 +26,23 @@ head =  {
 def http_util(url,head):
     res = urllib.request.urlopen(urllib.request.Request(url, None,head)).read().decode('utf-8')
     return res
+
+# 登录
+def login_qzone(driver):
+    print("##### start get Login Page #####")
+    # 切换账号密码登录
+    time.sleep(2)
+    driver.switch_to.frame('loginFrame')
+    driver.find_element_by_id("switcher_plogin").click()
+    # 等待Dom 加载
+    time.sleep(2)
+    driver.find_element_by_id('u').send_keys('------')
+    time.sleep(2)
+    driver.find_element_by_id('p').send_keys('------')
+    time.sleep(4)
+    driver.find_element_by_id("login_button").click()
+    print("#### Login Success ####")
+
 # 获取‘南宫鸢泽’关联的所有歌单
 def user_base_list():
     res =  http_util(base_url + music_list_url,head)
@@ -33,18 +57,61 @@ def music_list_hot():
     res = http_util(base_url + list_info_url + str(id), head)
     temp = json.loads(res)['result']
     comment_list = {}
+    pic_list = {}
+    artis_list = {}
+    music_name = {}
     if len(temp['tracks']) > 0:
         for i in temp['tracks']:
             comment_list[i['id']] = (i['commentThreadId'])
-    return comment_list
+            pic_list[i['id']] = (i['album']['picUrl'])
+            music_name[i['id']] = (i['album']['name'])
+            # 暂时只取一个歌手
+            artis_list[i['id']] = (i['album']['artists'][0]['name'])
+    return comment_list,artis_list,pic_list,music_name
 # 随机取一首歌，并加载热评，随机选取一条
 def get_end():
-    comment_list = music_list_hot()
+    listKL = music_list_hot()
+    comment_list = listKL[0]
+    pic_list = listKL[2]
+    artis_list = listKL[1]
+    music_name = listKL[3]
     id = random.choice(list(comment_list.keys()))
     res = http_util(str(base_url + commet_url).format(comment_list[id],comment_list[id]) , head)
     temp = json.loads(res)['hotComments']
-    return random.choice(temp)['content'],id
-print(get_end())
+    if len(temp) is 0:
+        raise RuntimeError('#### 没有热评')
+    return random.choice(temp)['content'],id,pic_list[id],artis_list[id],music_name[id]
 
+# --------
+def ROOIKE():
+    # 获取分享内容
+    results = get_end()
+    # 组装分享
+    if len(results[0]) > 120:
+        raise RuntimeError('#### 热评的B话太多了')
+    request_map['desc'] = results[0]
+    request_map['url'] = "https://music.163.com/song?id=" + str(results[1]) + "&userid=97752165&from=qzone"
+    request_map['title'] = "分享单曲：" + results[4]
+    request_map['summary'] = results[3]
+    request_map['pics'] = results[2] + "?imageView&thumbnail=120y120"
+    urls = qzone_url + urllib.parse.urlencode(request_map)
 
+    driver = webdriver.Chrome()
+    driver.get(urls)
+    # 获取登录session
+    driver.find_element_by_id("changeAccounts").click()
+    login_qzone(driver)
+    # 跳出登录弹窗
+    driver.switch_to.default_content()
+    print("##### start Push #####")
+    time.sleep(5)
+    driver.find_element_by_id("postButton").click()
+    print("##### Success Push ####")
 
+# 遭遇异常再来一次..
+while True:
+        try:
+            ROOIKE()
+            break
+        except ValueError:
+            print("遭遇了异常和不测，再推一遍")
